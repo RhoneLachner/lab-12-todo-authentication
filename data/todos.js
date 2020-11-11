2ee36bf45400f92019cdfc24114432951773416c
Rhone Lachner <fernandclay@gmail.com>
2020-11-10 20:17:30 -0800

    fixed small typos, postman not working

--- data/todos.js
+++ data/todos.js
@@ -3,16 +3,19 @@ module.exports = [
     id: 3,
     task: 'wash the dishes',
     completed: false,
+    owner_id: 1
   },
   {
     id: 2,
     task: 'wash car',
     completed: false,
+    owner_id: 1
   },
   {
     id: 1,
     task: 'vacuum',
     completed: false,
-  },
+    owner_id: 1
+  }
 ];
   

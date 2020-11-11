f0d95072abe2ee3dcd1a3519b04a531f79329f01
Rhone Lachner <fernandclay@gmail.com>
2020-11-10 19:49:53 -0800

    updated get and files with todo

---
+++ data/todos.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 3,
+    task: 'wash the dishes',
+    completed: false,
+  },
+  {
+    id: 2,
+    task: 'wash car',
+    completed: false,
+  },
+  {
+    id: 1,
+    task: 'vacuum',
+    completed: true,
+  }
+];
+  
